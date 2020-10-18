3e5936a33ee362d198dd41e26895d799b551a9f7
Kevin Robb <kevin@js-x.com>
2020-10-17 22:09:09 -0500

    Switch to python. Delete cpp files. Define functions and variables in kf.py.

--- sim_ws/src/capstone/src/kf.py
+++ sim_ws/src/capstone/src/kf.py
@@ -3,11 +3,52 @@
 import rospy
 import time
 
+# init flag
 initialized = False
 
-def timer_callback(event):
-    global initialized
+# collection of state variables' current estimation
+State = []
+# collection of measurement values
+Measurements = []
+# collection of predictions for state variables at next timestep
+Predictions = []
+
+## Uncertainties
+meas_uncertainty = None
+est_uncertainty = None
+
+kalman_gain = None
+
+
+def initialize():
+    global initialized, est_uncertainty, State
+    # set estimate uncertainty initial guess
+    # set system state initial guess
     initialized = True
+
+def predict():
+    global State
+    # calculate predicted state for next iteration using dynamic model
+    # extrapolate the estimate uncertainty
+
+def measure():
+    global meas_uncertainty, Measurements
+    # input measurement uncertainty
+    # input measured values
+
+def update():
+    global kalman_gain, State, est_uncertainty
+    # calculate kalman gain
+    # estimate the current state using the state update equation
+    # update the current estimate uncertainty
+
+def timer_callback(event):
+    if not initialized:
+        initialize()
+    else:
+        predict()
+        measure()
+        update()
 
 def main():
     # initalize the node in ROS
