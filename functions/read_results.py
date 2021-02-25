80ddc94deee1c854b56f0bdee8800e9b7ab8bc82
Kevin Robb <kevin@js-x.com>
2021-02-24 22:09:34 -0600

    Correctly reading in file and storing as dict

--- functions/read_results.py
+++ functions/read_results.py
@@ -2,9 +2,10 @@
 
 def read_file():
     # bring in results.txt as a list of strings for each line
-    # TODO
-    results = []
-    # assume it is stored in results, and empty lines are skipped
+    # open file in read mode.
+    file1=open("results.txt", "r+")
+    results = file1.readlines()
+    # assume empty lines are skipped
     
     # interpret the important elements, and write to dictionary.
     keys, values = [], []
@@ -37,10 +38,12 @@ def read_file():
     elements["Seed"] = int(elements["Seed"])
     obs_settings = {"x1.00":"normal", "x15.00":"none", "x0.10":"hard"}
     noise_settings = {"x10.00":"none", "x1.00":"reduced", "x0.60":"realistic"}
-    elements["Obstacle Type"] = obs_settings[elements["Obstacle Bonus"]]
-    elements["Noise Type"] = noise_settings[elements["Noise Bonus"]]
+    elements["Obstacle Type"] = obs_settings[elements["Obstacles Bonus"][:-1]]
+    elements["Noise Type"] = noise_settings[elements["Noise Bonus"][:-1]]
     # TODO do something with this stuff
+    print(elements)
 
+read_file()
 
 """
 Team WickedSlickRobotics
@@ -58,4 +61,33 @@ Waypoint 1 reached: x0.80
 Waypoint 2 reached: x0.80
 Waypoint 3 reached: x0.80
 44% Damaged: x5.01
+"""
+
+""" from my decision tree, reading from file
+def file_to_tree(self, filename:str=None) -> Node:
+        # read from a file and store the contents as a tree
+        if filename is None:
+            filename = self.filename
+        # open file in read mode.
+        file1=open("tree_storage/" + filename + ".txt", "r+")
+        # read each line & return as each a string element in a list.
+        file_content = file1.readlines()
+        # initialize array to store data from file.
+        self.arr = [None for i in range(len(file_content))]
+        # go through and parse each line as a row in arr.
+            # -2,None,None,None: no node.
+            # 1,int,None,None,int: terminal nodes.
+            # 1,int,int,float,None: other nodes.
+        # make sure to skip None rows (denoted "-" in file).
+        for i in range(len(file_content)):
+            row_str = file_content[i].split(",")
+            if int(row_str[0]) == -2: # no node. leave as None.
+                continue
+            elif row_str[2] == "None": # terminal node.
+                self.arr[i] = [int(row_str[1]),None,None,int(row_str[4])]
+            else: # non-terminal node.
+                self.arr[i] = [int(row_str[1]),int(row_str[2]),float(row_str[3]), None]
+        # convert arr to tree and return
+        self.root = self.arr_to_tree()
+        return self.root
 """
