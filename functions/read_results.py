d6ee1fe6a03a40c473899b4172cd1bc3f06ac865
Kevin Robb <kevin@js-x.com>
2021-02-24 22:35:53 -0600

    Results are output to aggregate CSV file in easy to process format.

--- functions/read_results.py
+++ functions/read_results.py
@@ -1,9 +1,12 @@
 # This file will read results.txt after it is created by the simulator finishing a run.
+from os import stat
+header = ["seed", "obstacle_type", "noise_type", "time", "score", "damage", "num_waypoints"]
+h_keys = ["Seed", "Obstacle Type", "Noise Type", "Time", "Score", "Damage", "Number of waypoints reached"]
 
 def read_file():
     # bring in results.txt as a list of strings for each line
     # open file in read mode.
-    file1=open("results.txt", "r+")
+    file1 = open("results.txt", "r+")
     results = file1.readlines()
     # assume empty lines are skipped
     
@@ -40,12 +43,27 @@ def read_file():
     noise_settings = {"x10.00":"none", "x1.00":"reduced", "x0.60":"realistic"}
     elements["Obstacle Type"] = obs_settings[elements["Obstacles Bonus"][:-1]]
     elements["Noise Type"] = noise_settings[elements["Noise Bonus"][:-1]]
-    # TODO do something with this stuff
-    print(elements)
 
-read_file()
+    return elements
 
-"""
+def write_file(elements):
+    filepath = "results/" + str(elements["Seed"]) + ".csv"
+    # don't overwrite with each statement (a=append).
+    file2 = open(filepath, "a+")
+    if stat(filepath).st_size == 0:
+        # if this is a new file, add the header
+        file2.write(",".join(header) + "\n")
+    # add the row for this round of results
+    row = ",".join([str(elements[k]) for k in h_keys])
+    file2.write(row + "\n")
+    file2.close()
+
+el = read_file()
+print(el)
+write_file(el)
+
+
+""" # Sample results.txt
 Team WickedSlickRobotics
 Date: 2021-02-23 17:50:17 -06
 Seed: 76168294
@@ -61,33 +79,4 @@ Waypoint 1 reached: x0.80
 Waypoint 2 reached: x0.80
 Waypoint 3 reached: x0.80
 44% Damaged: x5.01
-"""
-
-""" from my decision tree, reading from file
-def file_to_tree(self, filename:str=None) -> Node:
-        # read from a file and store the contents as a tree
-        if filename is None:
-            filename = self.filename
-        # open file in read mode.
-        file1=open("tree_storage/" + filename + ".txt", "r+")
-        # read each line & return as each a string element in a list.
-        file_content = file1.readlines()
-        # initialize array to store data from file.
-        self.arr = [None for i in range(len(file_content))]
-        # go through and parse each line as a row in arr.
-            # -2,None,None,None: no node.
-            # 1,int,None,None,int: terminal nodes.
-            # 1,int,int,float,None: other nodes.
-        # make sure to skip None rows (denoted "-" in file).
-        for i in range(len(file_content)):
-            row_str = file_content[i].split(",")
-            if int(row_str[0]) == -2: # no node. leave as None.
-                continue
-            elif row_str[2] == "None": # terminal node.
-                self.arr[i] = [int(row_str[1]),None,None,int(row_str[4])]
-            else: # non-terminal node.
-                self.arr[i] = [int(row_str[1]),int(row_str[2]),float(row_str[3]), None]
-        # convert arr to tree and return
-        self.root = self.arr_to_tree()
-        return self.root
 """
